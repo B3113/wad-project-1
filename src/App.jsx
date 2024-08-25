fd8cd0c046f28ae42bd114db4bc5545de882cb8f
Belle <bellebt2019@gmail.com>
2024-08-25 19:45:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import HighlightedCars from "./pages/HighlightedCars";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/highlighted" element={<HighlightedCars />} />
-      </Routes>
-    </Router>
+    <>
+      <div id="sidebar">
+        <nav>
+          <a href={`/`}>Dashboard</a>
+          <a href={`/highlighted`}>Highlight</a>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import HighlightedCars from "./pages/HighlightedCars.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/highlighted", element: <HighlightedCars /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </StrictMode>
 );
